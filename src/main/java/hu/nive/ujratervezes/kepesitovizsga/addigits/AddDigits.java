9ce46f6ff8618c70fcc493e21a8692da578a91b7
GergelyMihalyi <33895800+GergelyMihalyi@users.noreply.github.com>
2021-04-29 10:22:07 +0200

    Add digits solution

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -1,4 +1,19 @@
 package hu.nive.ujratervezes.kepesitovizsga.addigits;
 
 public class AddDigits {
+
+    public int addDigits(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+        int sum = 0;
+        char character;
+        for (int i = 0; i < input.length(); i++) {
+            character = input.charAt(i);
+            if (Character.isDigit(character)) {
+                sum += Character.getNumericValue(character);
+            }
+        }
+        return sum;
+    }
 }
